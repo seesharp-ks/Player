0078ede845ef995c80a59ceed19c0c1e78bd7c66
Nero <90371154+seesharp-ks@users.noreply.github.com>
2021-11-15 00:26:29 +0300

    horizontal & vertical orientation support
    Temporary: Double-Click on the Speed Multiplier to enable the feature

---
+++ AdditionalWindows/AboutWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace NovemberPlayer.AdditionalWindows
+{
+    /// <summary>
+    /// Логика взаимодействия для AboutWindow.xaml
+    /// </summary>
+    public partial class AboutWindow : Window
+    {
+        public AboutWindow()
+        {
+            InitializeComponent();
+            Description.Text = "Программа 'Видеоплеер'\n" +
+                "Курсовая работа по теме №19.\"Разработка мобильного приложения «Видеопроигрыватель».\"\n" +
+                "Начало: 20.09.2021\n" +
+                "Версия:\t11.2021\n" +
+                "Разработал:\tИванов А.А.\n" +
+                "Руководитель:\tТокарь И.А.\n" +
+                "Колледж связи №54 - 2021";
+        }//палитра https://lospec.com/palette-list/2-bit-blue
+
+        private void Accept_Click(object sender, RoutedEventArgs e) { this.Close(); }
+    }
+}
